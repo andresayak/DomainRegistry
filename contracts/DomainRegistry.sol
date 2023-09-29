6aa97dfd016c437c219558e12a529d4e05e53231
Andriy Saiak <andresayak@gmail.com>
2023-09-29 19:45:44 +0300

    fixed problems - remove faker lib - added methods for fetch domains by owner - optimized owners

--- contracts/DomainRegistry.sol
+++ contracts/DomainRegistry.sol
@@ -1,67 +1,100 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-struct DomainRecord {
-  address owner;
-  string ipAddress;
-}
+    struct DomainRecord {
+        address owner;
+        uint index;
+    }
 
 contract DomainRegistry {
-  uint public lockAmount;
-  bool internal locked;
-  mapping(string => DomainRecord) public registry;
+    uint public lockAmount;
+    bool internal locked;
+    mapping(string => DomainRecord) public registry;
+    mapping(address => string[]) public registryByOwner;
 
-  modifier noReentrant() {
-    require(!locked, "No re-entrancy");
-    locked = true;
-    _;
-    locked = false;
-  }
-
-  modifier notDots(bytes memory _domainBytes) {
-    bytes memory _dotBytes = bytes(".");
-
-    require(_domainBytes.length != 0, "empty domain");
-
-    for (uint i = 0; i < _domainBytes.length; i++) {
-      if (_domainBytes[i] == _dotBytes[0]) {
-        require(false, "wrong domain format");
-      }
+    modifier noReentrant() {
+        require(!locked, "No re-entrancy");
+        locked = true;
+        _;
+        locked = false;
     }
-    _;
-  }
 
-  event DomainReserved(string domain, string ipAddress, address indexed sender);
-  event DomainRemoved(string domain);
+    modifier notDots(bytes memory _domainBytes) {
+        bytes memory _dotBytes = bytes(".");
 
-  constructor(uint _lockAmount) {
-    lockAmount = _lockAmount;
-  }
+        require(_domainBytes.length != 0, "empty domain");
 
-  function reserveDomain(string memory _domain, string memory _ipAddress) external payable {
-    require(checkIsFreeDomain(_domain), "not free domain");
-    require(msg.value == lockAmount, "wrong value");
+        for (uint i = 0; i < _domainBytes.length; i++) {
+            if (_domainBytes[i] == _dotBytes[0]) {
+                require(false, "wrong domain format");
+            }
+        }
+        _;
+    }
 
-    registry[_domain] = DomainRecord(msg.sender, _ipAddress);
-    emit DomainReserved(_domain, _ipAddress, msg.sender);
-  }
+    modifier onlyOwner(string memory _domain) {
+        require(registry[_domain].owner == msg.sender, "wrong sender");
+        _;
+    }
 
-  function checkIsFreeDomain(string memory _domain) public view notDots(bytes(_domain)) returns (bool) {
-    return registry[_domain].owner == address(0);
-  }
+    event DomainReserved(string domain, address indexed sender);
+    event DomainRemoved(string domain);
 
-  function removeReservationDomain(string memory _domain) external noReentrant {
-    require(registry[_domain].owner == msg.sender, "wrong sender");
+    constructor(uint _lockAmount) {
+        lockAmount = _lockAmount;
+    }
 
-    (bool sent, ) = msg.sender.call{value: lockAmount}("");
-    require(sent, "failed to send Ether");
+    function reserveDomain(string memory _domain) external payable {
+        require(checkIsFreeDomain(_domain), "not free domain");
+        require(msg.value == lockAmount, "wrong value");
 
-    registry[_domain] = DomainRecord(address(0), "");
+        registry[_domain] = DomainRecord(msg.sender, registryByOwner[msg.sender].length);
+        registryByOwner[msg.sender].push(_domain);
+        emit DomainReserved(_domain, msg.sender);
+    }
 
-    emit DomainRemoved(_domain);
-  }
+    function checkIsFreeDomain(string memory _domain) public view notDots(bytes(_domain)) returns (bool) {
+        return registry[_domain].owner == address(0);
+    }
 
-  function resolveIp(string memory _domain) external view returns (string memory) {
-    return registry[_domain].ipAddress;
-  }
+    function removeReservationDomain(string memory _domain) external noReentrant onlyOwner(_domain) {
+        (bool sent,) = msg.sender.call{value: lockAmount}("");
+        require(sent, "failed to send Ether");
+
+        removeDomainOwnerByIndex(msg.sender, registry[_domain].index);
+        registry[_domain] = DomainRecord(address(0), 0);
+
+        emit DomainRemoved(_domain);
+    }
+
+    function getDomainsCountByOwner(address owner) external view returns (uint) {
+        return registryByOwner[owner].length;
+    }
+
+    function getDomainsByOwnerWithPagination(address _owner, uint _page, uint _limit) external view returns (string[] memory) {
+        uint _startIndex = (_page - 1) * _limit;
+        uint _endIndex = _startIndex + _limit;
+        if (_endIndex > registryByOwner[_owner].length) {
+            _endIndex = registryByOwner[_owner].length;
+        }
+        require(_startIndex >= 0 && _startIndex < _endIndex, "invalid indices");
+
+        string[] memory result = new string[](_endIndex - _startIndex);
+        for (uint i = _startIndex; i < _endIndex; i++) {
+            result[i - _startIndex] = registryByOwner[_owner][i];
+        }
+        return result;
+    }
+
+    function removeDomainOwnerByIndex(address _owner, uint _index) internal {
+        if (_index >= registryByOwner[_owner].length) return;
+
+        if (registryByOwner[_owner].length > _index + 1) {
+            string memory updateDomain = registryByOwner[_owner][registryByOwner[_owner].length - 1];
+            registryByOwner[_owner][_index] = updateDomain;
+            registry[updateDomain].index = _index;
+        }
+
+        registryByOwner[_owner].pop();
+    }
 }
--- test/1-DomainRegistry.js
+++ test/1-DomainRegistry.js
@@ -1,143 +1,110 @@
-const {
-  loadFixture,
-} = require('@nomicfoundation/hardhat-toolbox/network-helpers');
+const { loadFixture } = require('@nomicfoundation/hardhat-toolbox/network-helpers');
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
-const { faker } = require('@faker-js/faker');
 
-describe('DomainRegistry', function() {
-  const deployOneYearLockFixture = async (
-    lockAmount = 1_000_000_000, // 1 Gwei
-  ) => {
+describe('DomainRegistry', function () {
+  let contract, owner, otherAccount;
+  const lockAmount = 1_000_000_000;
 
-    const [owner, otherAccount] = await ethers.getSigners();
+  const deployContract = async () => {
+    [owner, otherAccount] = await ethers.getSigners();
 
     const Contract = await ethers.getContractFactory('DomainRegistry');
-    const contract = await Contract.deploy(lockAmount);
+    contract = await Contract.deploy(lockAmount);
 
     return { contract, lockAmount, owner, otherAccount };
   };
 
-  const successReserveDomain = async ({ contract, lockAmount, domain = faker.internet.domainSuffix() }) => {
-    const ipv4 = faker.internet.ipv4();
-
+  const successReserveDomain = async ({ contract, lockAmount, domain }) => {
     expect(await contract.checkIsFreeDomain(domain)).to.be.true;
 
-    await expect(contract.reserveDomain(domain, ipv4, {
-      value: lockAmount,
-    })).not.to.be.reverted;
+    await expect(
+      contract.reserveDomain(domain, {
+        value: lockAmount,
+      }),
+    ).not.to.be.reverted;
     return {
-      domain, ipv4,
+      domain,
     };
   };
 
-  describe('Deployment', function() {
-    it('Should set the right lockAmount', async function() {
-      const { contract, lockAmount } = await loadFixture(deployOneYearLockFixture);
+  beforeEach(async () => loadFixture(deployContract));
 
+  describe('Deployment', function () {
+    it('Should set the right lockAmount', async function () {
       expect(await contract.lockAmount()).to.equal(lockAmount);
     });
 
-    describe('Adding domain', function() {
-      it('Should reserve domain and store the funds to lock', async function() {
-        const { contract, lockAmount, owner } = await loadFixture(deployOneYearLockFixture);
-
-        const domain = faker.internet.domainSuffix();
-        const ipv4 = faker.internet.ipv4();
+    describe('Adding domain', function () {
+      it('Should reserve domain and store the funds to lock', async function () {
+        const domain = 'com';
 
         expect(await contract.checkIsFreeDomain(domain)).to.be.true;
 
-        const tx = contract.reserveDomain(domain, ipv4, {
+        const tx = contract.reserveDomain(domain, {
           value: lockAmount,
         });
-        await expect(tx).to.emit(contract, 'DomainReserved')
-          .withArgs(domain, ipv4, owner.address);
+        await expect(tx).to.emit(contract, 'DomainReserved').withArgs(domain, owner.address);
 
-        await expect(tx)
-          .to.changeEtherBalances(
-            [contract, owner],
-            [lockAmount, -lockAmount],
-          );
+        await expect(tx).to.changeEtherBalances([contract, owner], [lockAmount, -lockAmount]);
 
-        expect(await ethers.provider.getBalance(contract.target)).to.equal(
-          lockAmount,
-        );
+        expect(await ethers.provider.getBalance(contract.target)).to.equal(lockAmount);
 
         expect(await contract.checkIsFreeDomain(domain)).to.be.false;
-        expect(await contract.resolveIp(domain)).to.be.equal(ipv4);
       });
 
-      it('Should reserve many domains', async function() {
-        const { contract, lockAmount } = await loadFixture(deployOneYearLockFixture);
-        await successReserveDomain({contract, lockAmount, domain: 'aaa'});
-        await successReserveDomain({contract, lockAmount, domain: 'bbb'});
+      it('Should reserve many domains', async function () {
+        await successReserveDomain({ contract, lockAmount, domain: 'aaa' });
+        await successReserveDomain({ contract, lockAmount, domain: 'bbb' });
       });
 
-      it('Should revert with the right error if domain empty', async function() {
-        const { contract, lockAmount } = await loadFixture(deployOneYearLockFixture);
-
+      it('Should revert with the right error if domain empty', async function () {
         const domain = '';
-        const ipv4 = faker.internet.ipv4();
 
-        await expect(contract.reserveDomain(domain, ipv4, {
-          value: lockAmount,
-        })).to.be.revertedWith(
-          'empty domain',
-        );
+        await expect(
+          contract.reserveDomain(domain, {
+            value: lockAmount,
+          }),
+        ).to.be.revertedWith('empty domain');
       });
 
-      it('Should revert with the right error if domain format wrong', async function() {
-        const { contract, lockAmount } = await loadFixture(deployOneYearLockFixture);
+      it('Should revert with the right error if domain format wrong', async function () {
+        const domain = 'bbb.aaa';
 
-        const domain = faker.internet.domainName();
-        const ipv4 = faker.internet.ipv4();
-
-        await expect(contract.reserveDomain(domain, ipv4, {
-          value: lockAmount,
-        })).to.be.revertedWith(
-          'wrong domain format',
-        );
+        await expect(
+          contract.reserveDomain(domain, {
+            value: lockAmount,
+          }),
+        ).to.be.revertedWith('wrong domain format');
       });
 
-      it('Should revert with the right error if wrong funds', async function() {
-        const { contract } = await loadFixture(deployOneYearLockFixture);
+      it('Should revert with the right error if wrong funds', async function () {
+        const domain = 'aaa';
 
-        const domain = faker.internet.domainSuffix();
-        const ipv4 = faker.internet.ipv4();
-
-        await expect(contract.reserveDomain(domain, ipv4, {
-          value: 0,
-        })).to.be.revertedWith(
-          'wrong value',
-        );
+        await expect(
+          contract.reserveDomain(domain, {
+            value: 0,
+          }),
+        ).to.be.revertedWith('wrong value');
         expect(await contract.checkIsFreeDomain(domain)).to.be.true;
       });
     });
 
-    describe('Removing domain', function() {
-      it('Should remove domain reservation and return the funds', async function() {
-        const { contract, lockAmount, owner }
-          = await loadFixture(deployOneYearLockFixture);
-
-        const { domain } = await successReserveDomain({ contract, lockAmount });
+    describe('Removing domain', function () {
+      it('Should remove domain reservation and return the funds', async function () {
+        const domain = 'aaa';
+        await successReserveDomain({ contract, lockAmount, domain });
 
         const tx = contract.removeReservationDomain(domain);
-        await expect(tx)
-          .to.changeEtherBalances(
-            [owner, contract],
-            [lockAmount, -lockAmount],
-          );
-        await expect(tx).to.emit(contract, 'DomainRemoved')
-          .withArgs(domain);
+        await expect(tx).to.changeEtherBalances([owner, contract], [lockAmount, -lockAmount]);
+        await expect(tx).to.emit(contract, 'DomainRemoved').withArgs(domain);
 
         expect(await contract.checkIsFreeDomain(domain)).to.be.true;
         expect(await ethers.provider.getBalance(contract.target)).to.equal(0);
       });
 
-      it('Should revert with the right error if domain have\'t reserved', async function() {
-        const { contract, otherAccount } = await loadFixture(deployOneYearLockFixture);
-
-        const domain = faker.internet.domainSuffix();
+      it('Should revert with the right error if domain have\'t reserved', async function () {
+        const domain = 'aaa';
 
         expect(await contract.checkIsFreeDomain(domain)).to.be.true;
 
@@ -146,25 +113,50 @@ describe('DomainRegistry', function() {
         expect(await contract.checkIsFreeDomain(domain)).to.be.true;
       });
 
-      it('Should revert with the right error if wrong owner', async function() {
-        const { contract, lockAmount, otherAccount } = await loadFixture(deployOneYearLockFixture);
-
-        const { domain } = await successReserveDomain({ contract, lockAmount });
+      it('Should revert with the right error if wrong owner', async function () {
+        const domain = 'aaa';
+        await successReserveDomain({ contract, lockAmount, domain });
 
         await expect(contract.connect(otherAccount).removeReservationDomain(domain)).to.be.revertedWith('wrong sender');
 
         expect(await contract.checkIsFreeDomain(domain)).to.be.false;
       });
 
-      it('Should fail second try remove request', async function() {
-        const { contract, lockAmount } = await loadFixture(deployOneYearLockFixture);
-
-        const { domain } = await successReserveDomain({ contract, lockAmount });
+      it('Should fail second try remove request', async function () {
+        const domain = 'aaa';
+        await successReserveDomain({ contract, lockAmount, domain });
 
         await expect(contract.removeReservationDomain(domain)).not.to.be.reverted;
         await expect(contract.removeReservationDomain(domain)).to.be.reverted;
       });
+    });
 
+    describe('Domains pagination', function () {
+      const [domain1, domain2, domain3] = ['aaa', 'bbb', 'ccc'];
+
+      beforeEach(async () => {
+        await successReserveDomain({ contract, lockAmount, domain: domain1 });
+        await successReserveDomain({ contract, lockAmount, domain: domain2 });
+        await successReserveDomain({ contract, lockAmount, domain: domain3 });
+      });
+
+      it('Should fetch right ordered list', async function () {
+        expect(await contract.getDomainsCountByOwner(owner)).to.be.equal(3);
+        expect(await contract.getDomainsByOwnerWithPagination(owner, 1, 2)).to.have.ordered.members([domain1, domain2]);
+        expect(await contract.getDomainsByOwnerWithPagination(owner, 2, 2)).to.have.ordered.members([domain3]);
+      });
+
+      it('Should fetch true list after remove last in list domain', async function () {
+        await expect(contract.removeReservationDomain(domain3)).not.to.be.reverted;
+        expect(await contract.getDomainsCountByOwner(owner)).to.be.equal(2);
+        expect(await contract.getDomainsByOwnerWithPagination(owner, 1, 2)).to.have.ordered.members([domain1, domain2]);
+      });
+
+      it('Should fetch true list after remove first in list domain', async function () {
+        await expect(contract.removeReservationDomain(domain1)).not.to.be.reverted;
+        expect(await contract.getDomainsCountByOwner(owner)).to.be.equal(2);
+        expect(await contract.getDomainsByOwnerWithPagination(owner, 1, 2)).to.have.ordered.members([domain3, domain2]);
+      });
     });
   });
 });
