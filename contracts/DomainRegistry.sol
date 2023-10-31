2e5124e9e5e97b13e8479c19d7a0191d1afe038d
Andriy Saiak <andresayak@gmail.com>
2023-10-31 13:34:43 +0200

    merged main branch to current

--- contracts/DomainRegistry.sol
+++ contracts/DomainRegistry.sol
@@ -6,6 +6,8 @@ import "hardhat/console.sol";
 import './LibUtils.sol';
 import '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
 
+/// @author Andrii Saiak
+/// @title DomainRegistry - A smart contract for domain registration and management.
 contract DomainRegistry is OwnableUpgradeable {
     uint256[500] private __gap;
 
@@ -25,13 +27,6 @@ contract DomainRegistry is OwnableUpgradeable {
     uint public fee;
     mapping(address => uint) public rewards;
 
-    modifier noReentrant() {
-        require(!locked, 'No re-entrancy');
-        locked = true;
-        _;
-        locked = false;
-    }
-
     event DomainReserved(address indexed sender, string indexed domain, uint cost, uint createdAt, uint finishedAt);
     event DomainRemoved(address indexed sender, string indexed domain);
     event DomainContinue(address indexed sender, string indexed domain, uint cost, uint finishedAt);
@@ -40,12 +35,19 @@ contract DomainRegistry is OwnableUpgradeable {
     event PaymentPeriodChanged(uint paymentPeriod);
     event FeeChanged(uint fee);
     event WithdrawReward(address indexed sender, uint amount);
+    event AdditionalPriceChanged(address indexed sender, string indexed domain, uint amount);
 
-    function setFee(uint _fee) external onlyOwner {
-        fee = _fee;
-        emit FeeChanged(_fee);
+    modifier noReentrant() {
+        require(!locked, 'No re-entrancy');
+        locked = true;
+        _;
+        locked = false;
     }
 
+    /// @dev Initializes the DomainRegistry contract.
+    /// @param _mainPrice The main price for domain registration.
+    /// @param _treasure The address where funds are collected.
+    /// @param _paymentPeriod The payment period for the domain
     function initialize(uint _mainPrice, address _treasure, uint _paymentPeriod) public initializer {
         mainPrice = _mainPrice;
         treasure = _treasure;
@@ -53,48 +55,81 @@ contract DomainRegistry is OwnableUpgradeable {
         __Ownable_init(_msgSender());
     }
 
+    /// @notice Change the fee
+    /// @dev Only the owner of the contract can change the fee
+    /// @param _fee The new fee
+    function setFee(uint _fee) external onlyOwner {
+        fee = _fee;
+        emit FeeChanged(_fee);
+    }
+
+    /// @notice Change the address where funds are collected (treasure).
+    /// @dev Only the owner of the contract can change the treasure address.
+    /// @param _treasure The new address to receive funds.
     function changeTreasure(address _treasure) external onlyOwner {
         require(_treasure != address(0), 'wrong address');
         treasure = _treasure;
         emit TreasureChanged(_treasure);
     }
 
+    /// @notice Change the main price for domain registration.
+    /// @dev Only the owner of the contract can change the main price.
+    /// @param _mainPrice The new price for domain registration.
     function changeMainPrice(uint _mainPrice) external onlyOwner {
         mainPrice = _mainPrice;
         emit MainPriceChanged(_mainPrice);
     }
 
+    /// @notice Change the payment period for domain registration.
+    /// @dev Only the owner of the contract can change the payment period.
+    /// @param _paymentPeriod The new period (in seconds) for which domains will be registered.
     function changePaymentPeriod(uint _paymentPeriod) external onlyOwner {
         require(_paymentPeriod > 0, 'wrong payment period');
         paymentPeriod = _paymentPeriod;
         emit PaymentPeriodChanged(_paymentPeriod);
     }
 
+    /// @notice Get information about a registered domain.
+    /// @dev Allows users to retrieve information about a specific registered domain, including details such as the owner,
+    /// creation date, and expiration date.
+    /// @param _domain The domain name for which information is requested.
+    /// @return A DomainRecord struct containing information about the domain.
     function domainInfo(string memory _domain) external view returns (DomainRecord memory) {
         require(!_onlyFreeDomain(_domain), 'free domain');
         return registryByName[_domain];
     }
 
+    /// @notice Check if a domain is available for registration.
+    /// @dev Allows users to check whether a specific domain name is available for registration, meaning it has not
+    /// been reserved by another user and is not currently under reservation.
+    /// @param _domain The domain name to check for availability.
+    /// @return True if the domain is available for registration, false otherwise.
     function isFreeDomain(string memory _domain) external view returns (bool) {
         return _onlyFreeDomain(Utils.clearDomain(_domain));
     }
 
-    function _onlyFreeDomain(string memory _domain) public view returns (bool) {
-        return registryByName[_domain].owner == address(0) || block.timestamp > registryByName[_domain].finishedAt;
-    }
-
+    /// @notice Get the owner of a registered domain.
+    /// @dev Allows users to retrieve the address of the owner of a specific registered domain.
+    /// @param _domain The domain name for which the owner's address is requested.
+    /// @return The Ethereum address of the owner of the domain.
     function domainOwner(string memory _domain) external view returns (address) {
         return registryByName[_domain].owner;
     }
 
+    /// @notice Get the price of a domain registration.
+    /// @dev Allows users to retrieve the address of the owner of a specific registered domain.
+    /// @param _domain The domain name for which the price's address is requested.
+    /// @return price of the domain.
     function domainPrice(string memory _domain) external view returns (uint) {
         return registryByName[_domain].additionalPrice + mainPrice;
     }
 
-    function validateDomainOwner(string memory _domain) internal view {
-        require(registryByName[_domain].owner == _msgSender(), 'wrong sender');
-    }
-
+    /// @notice Reserve a domain for a specified number of periods.
+    /// @dev Allows users to reserve a domain by providing the domain name and the number of periods they want to
+    /// reserve it for. The cost of the reservation is calculated based on the main price and the specified number
+    /// of periods.
+    /// @param _domain The domain name to be reserved.
+    /// @param _periods The number of periods for which the domain will be reserved.
     function reserveDomain(string memory _domain, uint8 _periods, uint _additionalPrice) external payable {
         _domain = Utils.clearDomain(_domain);
         require(_onlyFreeDomain(_domain), 'not free domain');
@@ -104,7 +139,7 @@ contract DomainRegistry is OwnableUpgradeable {
             require(!_onlyFreeDomain(_parentDomain), 'parent domain is free');
         }
         uint _createdAt = block.timestamp;
-        (uint _finishedAt, uint _cost) = paymentProcessing(_parentDomain, mainPrice, _periods, _createdAt);
+        (uint _finishedAt, uint _cost) = _paymentProcessing(_parentDomain, mainPrice, _periods, _createdAt);
 
         DomainRecord memory _record;
         _record.owner = _msgSender();
@@ -116,21 +151,47 @@ contract DomainRegistry is OwnableUpgradeable {
         emit DomainReserved(_msgSender(), _domain, _cost, _createdAt, _finishedAt);
     }
 
+    /// @notice Change the additional price for domain registration.
+    /// @dev Only owner of the domain can change the additional price.
+    /// @param _domain The domain name for which the additional price will be changed.
+    /// @param _additionalPrice The new additional price for domain registration.
+    function changeAdditionPrice(string memory _domain, uint _additionalPrice) external {
+        _domain = Utils.clearDomain(_domain);
+        require(!_onlyFreeDomain(_domain), 'free domain');
+        registryByName[_domain].additionalPrice = _additionalPrice;
+
+        emit AdditionalPriceChanged(_msgSender(), _domain, _additionalPrice);
+    }
+
+    /// @notice Continue the reservation of an existing domain for additional periods.
+    /// @dev Allows the owner of a previously reserved domain to extend the reservation by specifying the number of
+    /// additional periods. The cost of the extension is calculated based on the main price and the specified number
+    /// of periods.
+    /// @param _domain The domain name for which the reservation will be extended.
+    /// @param _periods The number of additional periods for which the domain will be reserved.
     function continueDomain(string memory _domain, uint8 _periods) external payable {
         _domain = Utils.clearDomain(_domain);
-        validateDomainOwner(_domain);
+        _validateDomainOwner(_domain);
         require(_periods > 0, 'wrong periods');
         string memory _parentDomain = Utils.parentDomain(_domain);
         DomainRecord storage _record = registryByName[_domain];
 
         uint _createdAt = _record.finishedAt;
-        (uint _finishedAt, uint _cost) = paymentProcessing(_parentDomain, mainPrice, _periods, _createdAt);
+        (uint _finishedAt, uint _cost) = _paymentProcessing(_parentDomain, mainPrice, _periods, _createdAt);
 
         _record.finishedAt = _finishedAt;
         emit DomainContinue(_msgSender(), _domain, _cost, _finishedAt);
     }
 
-    function paymentProcessing(string memory _parentDomain, uint _price, uint8 _periods, uint _periodStartedAt) internal returns (uint, uint) {
+    /// @notice Process payments for domain reservations and extensions.
+    /// @dev Internal function used to handle the payment processing for domain reservations and extensions.
+    /// It calculates the cost of the reservation, verifies the provided value, and transfers funds to the contract's
+    /// treasury address.
+    /// @param _price The price of a single period of domain registration.
+    /// @param _periods The number of periods for which the domain will be reserved or extended.
+    /// @param _periodStartedAt The timestamp when the current period started.
+    /// @return The timestamp when the reservation or extension period will end and the total cost of the transaction.
+    function _paymentProcessing(string memory _parentDomain, uint _price, uint8 _periods, uint _periodStartedAt) internal returns (uint, uint) {
         uint _cost = _price * _periods;
 
         require(msg.value == _cost, 'wrong value');
@@ -152,6 +213,8 @@ contract DomainRegistry is OwnableUpgradeable {
         return (_finishedAt, _cost);
     }
 
+    /// @notice get reward for domain registration
+    /// @dev Allows the owner of a contract or parent domain to withdraw reward for domain registration
     function withdrawReward() external noReentrant {
         uint _reward = rewards[_msgSender()];
         require(_reward > 0, 'no reward');
@@ -161,5 +224,22 @@ contract DomainRegistry is OwnableUpgradeable {
 
         rewards[_msgSender()] = 0;
         emit WithdrawReward(_msgSender(), _reward);
+    }
+
+    /// @notice Check if a domain is free and available for registration.
+    /// @dev Internal function used to determine if a specific domain is available for registration, meaning it either
+    /// has no owner or its reservation has expired.
+    /// @param _domain The domain name to check for availability.
+    /// @return True if the domain is free and available for registration, false otherwise.
+    function _onlyFreeDomain(string memory _domain) internal view returns (bool) {
+        return registryByName[_domain].owner == address(0) || block.timestamp > registryByName[_domain].finishedAt;
+    }
+
+    /// @notice Verify the ownership of a registered domain.
+    /// @dev Internal function used to confirm that the caller is the rightful owner of a specific registered domain.
+    /// It checks if the provided Ethereum address matches the owner's address of the domain.
+    /// @param _domain The domain name to validate ownership for.
+    function _validateDomainOwner(string memory _domain) internal view {
+        require(registryByName[_domain].owner == _msgSender(), 'wrong sender');
     }
 }
