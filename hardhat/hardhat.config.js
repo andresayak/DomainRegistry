1ce21b9fb04ad537caad20499899090a5fa76697
Andriy Saiak <andresayak@gmail.com>
2023-11-06 23:45:26 +0200

    fixed CI/CD issues

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -2,15 +2,15 @@ require('@nomicfoundation/hardhat-toolbox');
 require('hardhat-gas-reporter');
 require('@openzeppelin/hardhat-upgrades');
 
-const { config: dotEnvConfig } = require('dotenv');
-dotEnvConfig({ path: __dirname + '/.env' });
+const {config: dotEnvConfig} = require('dotenv');
+dotEnvConfig({path: __dirname + '/.env'});
 
 const mnemonic = process.env.MNEMONIC || 'test test test test test test test test test test test junk';
 
 const accounts = {
   mnemonic,
-  path: "m/44'/60'/0'/0",
-  initialIndex: 0,
+  path: 'm/44\'/60\'/0\'/0',
+  initialIndex: 0
 };
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -20,24 +20,24 @@ module.exports = {
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200,
-      },
-    },
+        runs: 200
+      }
+    }
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS ? true : false,
   },
   networks: {
     hardhat: {
-      accounts,
-      forking: {
-        url: process.env.ETHEREUM_SEPOLIA_PROVIDER_URL
-      }
+      accounts
+      //forking: {
+      //  url: process.env.ETHEREUM_SEPOLIA_PROVIDER_URL
+      //}
     },
     ethereum_sepolia: {
       chainId: 11155111,
       url: process.env.ETHEREUM_SEPOLIA_PROVIDER_URL,
-      accounts,
-    },
-  },
+      accounts
+    }
+  }
 };
